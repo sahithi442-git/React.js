6a9a089795781e297804066d2b8a2dd5bccaa4f0
sahithi442-git <2211cs010442@mallareddyuniversity.ac.in>
2025-02-08 11:37:26 +0530

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,15 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import Cbc from './Component/Cbc.jsx'
-import Fbc from './Component/Fbc.jsx'
-import Demo from './Component/Demo.jsx'
-import Parent from './component/Parent.jsx'
+// import External from './styles/External'
+import Ref from './useRef/Ref'
+// import './index.css'
+// import State from './state/State.jsx'
+// import Inline from './styles/Inline.jsx'
+//import Cbc from './components/Cbc.jsx'
+//import Demo from './components/Demo.jsx'
+// import Parent from './props/Parent.jsx'
+// import "../src/styles/globally.css"
+// import Comp1 from './styles/Comp1'
+// import Comp2 from './styles/Comp2'
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Cbc />
-    <Fbc />
-    <Demo />
-    <Parent />
+    {/*<Cbc/>}
+    {/* <Demo/> */}
+    {/* <Parent/> */}
+    {/* <Inline/> */}
+    {/* <State/> */}
+    {/* <Comp1/>
+    <Comp2/> */}
+    {/* <External/> */}
+    <Ref />
   </StrictMode>,
 )
